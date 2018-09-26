4e67843ff2859ca1c2d8992f837213b3514c2db3
leeyunjea <yunjea0312@naver.com>
2018-09-26 18:55:57 +0900

    Update Image to Picaso

--- app/src/main/java/org/androidtown/foodtruckgram/Adapter/SellerMenuListAdapter.java
+++ app/src/main/java/org/androidtown/foodtruckgram/Adapter/SellerMenuListAdapter.java
@@ -16,6 +16,8 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.squareup.picasso.Picasso;
+
 import org.androidtown.foodtruckgram.Activity.SellerMenuEditActivity;
 import org.androidtown.foodtruckgram.Info.FoodTruckInfo;
 import org.androidtown.foodtruckgram.Info.MenuInfo;
@@ -33,12 +35,12 @@ public class SellerMenuListAdapter extends RecyclerView.Adapter<SellerMenuListAd
         // Your holder should contain a member variable
         // for any view that will be set as you render a row
         public ImageView menuImage;
-        public TextView name,price,introduce,menuImageURI;
-        public ImageButton remove,edit;
+        public TextView name, price, introduce, menuImageURI;
+        public ImageButton remove, edit;
         private SellerMenuListAdapter adapter;
         private Context context;
 
-        String serverURL = "http://"+ HttpClient.ipAdress+HttpClient.serverPort + HttpClient.urlBase + "/s/updateMenu";
+        String serverURL = "http://" + HttpClient.ipAdress + HttpClient.serverPort + HttpClient.urlBase + "/s/updateMenu";
 
         public ViewHolder(final Context context, View itemView, final SellerMenuListAdapter adapter, final FoodTruckInfo foodTruckInfo) {
             // Stores the itemView in a public final member variable that can be used
@@ -48,13 +50,13 @@ public class SellerMenuListAdapter extends RecyclerView.Adapter<SellerMenuListAd
             this.context = context;
             this.adapter = adapter;
 
-            menuImage = (ImageView)itemView.findViewById(R.id.menuImage_imageView);
-            name = (TextView)itemView.findViewById(R.id.menuName_textView);
-            price = (TextView)itemView.findViewById(R.id.menuPrice_textView);
-            introduce = (TextView)itemView.findViewById(R.id.menuIntroduce_textView);
-            menuImageURI = (TextView)itemView.findViewById(R.id.menuImageURI);
+            menuImage = (ImageView) itemView.findViewById(R.id.menuImage_imageView);
+            name = (TextView) itemView.findViewById(R.id.menuName_textView);
+            price = (TextView) itemView.findViewById(R.id.menuPrice_textView);
+            introduce = (TextView) itemView.findViewById(R.id.menuIntroduce_textView);
+            menuImageURI = (TextView) itemView.findViewById(R.id.menuImageURI);
 
-            remove = (ImageButton)itemView.findViewById(R.id.menuItemRemoveBtn);
+            remove = (ImageButton) itemView.findViewById(R.id.menuItemRemoveBtn);
             remove.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
@@ -66,19 +68,19 @@ public class SellerMenuListAdapter extends RecyclerView.Adapter<SellerMenuListAd
                 }
             });
 
-            edit = (ImageButton)itemView.findViewById(R.id.menuItemEditBtn);
+            edit = (ImageButton) itemView.findViewById(R.id.menuItemEditBtn);
             edit.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
                     int position = getLayoutPosition();
                     Toast.makeText(context, name.getText() + Integer.toString(position) + "Edit", Toast.LENGTH_SHORT).show();
                     Intent intent = new Intent(context, SellerMenuEditActivity.class);
-                    intent.putExtra("foodTruckInfo",foodTruckInfo);
+                    intent.putExtra("foodTruckInfo", foodTruckInfo);
                     intent.putExtra("menuImage", menuImageURI.getText());
-                    intent.putExtra("menuName",name.getText());
-                    intent.putExtra("menuPrice",price.getText());
-                    intent.putExtra("menuIntroduce",introduce.getText());
-                    intent.putExtra("serverURL",serverURL);
+                    intent.putExtra("menuName", name.getText());
+                    intent.putExtra("menuPrice", price.getText());
+                    intent.putExtra("menuIntroduce", introduce.getText());
+                    intent.putExtra("serverURL", serverURL);
                     context.startActivity(intent);
 
                 }
@@ -101,7 +103,7 @@ public class SellerMenuListAdapter extends RecyclerView.Adapter<SellerMenuListAd
     // Store the context for easy access
     private Context mContext;
     FoodTruckInfo foodTruckInfo;
-    String serverURL = "http://"+ HttpClient.ipAdress+HttpClient.serverPort + HttpClient.urlBase + "/s/deleteMenu";
+    String serverURL = "http://" + HttpClient.ipAdress + HttpClient.serverPort + HttpClient.urlBase + "/s/deleteMenu";
 
     // Pass in the contact array into the constructor
     public SellerMenuListAdapter(Context context, ArrayList<MenuInfo> menuList, FoodTruckInfo foodTruckInfo) {
@@ -125,7 +127,7 @@ public class SellerMenuListAdapter extends RecyclerView.Adapter<SellerMenuListAd
         View contactView = inflater.inflate(R.layout.item_seller_menu, parent, false);
 
         // Return a new holder instance
-        ViewHolder viewHolder = new ViewHolder(context, contactView, this,foodTruckInfo);
+        ViewHolder viewHolder = new ViewHolder(context, contactView, this, foodTruckInfo);
         return viewHolder;
     }
 
@@ -139,15 +141,16 @@ public class SellerMenuListAdapter extends RecyclerView.Adapter<SellerMenuListAd
         //viewHolder.menuImage.setImageResource(R.drawable.burger);  //menu image edit
         String base64 = menuInfo.getMenuImage();
         Log.i("Edit", "base64 = " + base64);
-        if(base64 != null && base64 != "") {
-            byte[] decodedString = Base64.decode(base64, Base64.NO_WRAP);
-            Bitmap bitmap = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
-            viewHolder.menuImage.setImageBitmap(bitmap);
-            Log.i("Edit", "menuImage = " + base64);
-        }
-        else {
+        if (base64 != null && base64 != "") {
+//            byte[] decodedString = Base64.decode(base64, Base64.NO_WRAP);
+//            Bitmap bitmap = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
+//            viewHolder.menuImage.setImageBitmap(bitmap);
+//            Log.i("Edit", "menuImage = " + base64);
+            Picasso.with(getContext()).load(base64).into(viewHolder.menuImage);
+        } else {
             viewHolder.menuImage.setImageResource(R.drawable.burger);
         }
+
 
         viewHolder.name.setText(menuInfo.getMenuName());
         viewHolder.price.setText(menuInfo.getMenuPrice());
@@ -155,7 +158,8 @@ public class SellerMenuListAdapter extends RecyclerView.Adapter<SellerMenuListAd
         Log.i("Edit", "introduce = " + menuInfo.getMenuIntroduce());
         viewHolder.menuImageURI.setText(menuInfo.getMenuImage());
 
-        Log.i("RecyclerView","name : "+ menuInfo.getMenuName() + " / price : "+menuInfo.getMenuPrice());
+        Log.i("RecyclerView", "name : " + menuInfo.getMenuName() + " / price : " + menuInfo.getMenuPrice());
+
 
     }
 
@@ -166,9 +170,9 @@ public class SellerMenuListAdapter extends RecyclerView.Adapter<SellerMenuListAd
     }
 
     public void removeItem(int p) {
-        Map<String,String> params = new HashMap<String,String>();
-        params.put("storeName",foodTruckInfo.getStoreName());
-        params.put("menuName",menuList.get(p).getMenuName());
+        Map<String, String> params = new HashMap<String, String>();
+        params.put("storeName", foodTruckInfo.getStoreName());
+        params.put("menuName", menuList.get(p).getMenuName());
 
         MenuDeleteDB menuDeleteDB = new MenuDeleteDB();
         menuDeleteDB.execute(params);
@@ -177,18 +181,18 @@ public class SellerMenuListAdapter extends RecyclerView.Adapter<SellerMenuListAd
         notifyItemRemoved(p);
     }
 
-    public void notifyData(FoodTruckInfo foodTruckInfo){
+    public void notifyData(FoodTruckInfo foodTruckInfo) {
         menuList = foodTruckInfo.getMenuList();
-        Log.i("Edit","notifyData()호출");
-        Log.i("Edit","foodTruckInfo.getMenuList().size() : "+foodTruckInfo.getMenuList().size());
+        Log.i("Edit", "notifyData()호출");
+        Log.i("Edit", "foodTruckInfo.getMenuList().size() : " + foodTruckInfo.getMenuList().size());
         notifyDataSetChanged();
     }
 
     class MenuDeleteDB extends AsyncTask<Map<String, String>, Integer, String> {
 
         @Override
-        protected String doInBackground(Map<String, String>...maps) {
-            HttpClient.Builder http = new HttpClient.Builder("POST",serverURL);
+        protected String doInBackground(Map<String, String>... maps) {
+            HttpClient.Builder http = new HttpClient.Builder("POST", serverURL);
             http.addAllParameters(maps[0]);
 
             HttpClient post = http.create();
@@ -210,6 +214,7 @@ public class SellerMenuListAdapter extends RecyclerView.Adapter<SellerMenuListAd
             super.onPostExecute(aVoid);
 
         }
+
 
     }
 }
