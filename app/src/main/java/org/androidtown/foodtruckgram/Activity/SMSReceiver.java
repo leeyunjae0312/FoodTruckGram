e2705234791c8dca8779654dec7aaa499dd898aa
leeyunjea <yunjea0312@naver.com>
2018-09-26 15:20:43 +0900

    Update SMSReceiver

--- app/src/main/java/org/androidtown/foodtruckgram/Activity/SMSReceiver.java
+++ app/src/main/java/org/androidtown/foodtruckgram/Activity/SMSReceiver.java
@@ -13,8 +13,6 @@ import java.util.Date;
 
 public class SMSReceiver extends BroadcastReceiver {
 
-    //private static final String SMS_RECEIVED = "android.provider.Telephony.SMS_RECEIVED";
-    //private static final String TAG = "SMSReceiver";
 
     private static SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm");
 
@@ -37,19 +35,19 @@ public class SMSReceiver extends BroadcastReceiver {
         }
 
 
-// SMS 수신 시간 확인
+        // SMS 수신 시간 확인
 
         Date curDate = new Date(smsMessage[0].getTimestampMillis());
 
         Log.d("문자 수신 시간", curDate.toString());
 
 
-// SMS 발신 번호 확인
+        // SMS 발신 번호 확인
 
         String origNumber = smsMessage[0].getOriginatingAddress();
 
 
-// SMS 메시지 확인
+        // SMS 메시지 확인
 
         String message = smsMessage[0].getMessageBody().toString();
 
@@ -58,20 +56,6 @@ public class SMSReceiver extends BroadcastReceiver {
 
     }
 
-//    // 액티비티로 메세지의 내용을 전달해줌
-//    private void sendToActivity(Context context, String sender, String contents, Date receivedDate){
-//        Intent intent = new Intent(context, SMSActivity.class);
-//
-//        // Flag 설정
-//        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK| Intent.FLAG_ACTIVITY_SINGLE_TOP| Intent.FLAG_ACTIVITY_CLEAR_TOP);
-//
-//        // 메세지의 내용을 Extra에 넣어줌
-//        intent.putExtra("sender", sender);
-//        intent.putExtra("contents", contents);
-//        intent.putExtra("receivedDate", format.format(receivedDate));
-//
-//        context.startActivity(intent);
-//    }
 
     private SmsMessage[] parseSmsMessage(Bundle bundle) {
         Object[] objs = (Object[]) bundle.get("pdus");
