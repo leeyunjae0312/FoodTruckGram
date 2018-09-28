7d03ec11bbef6ceceee79c891a048e4a75969919
leeyunjea <yunjea0312@naver.com>
2018-09-28 23:23:12 +0900

    Update Server URL to AWS

--- app/src/main/java/org/androidtown/foodtruckgram/Server/HttpClient.java
+++ app/src/main/java/org/androidtown/foodtruckgram/Server/HttpClient.java
@@ -18,7 +18,7 @@ import java.util.Map;
 public class HttpClient {
 
     private static final String WWW_FORM = "application/x-www-form-urlencoded";
-    public static final String ipAdress = "223.194.129.76";
+    public static final String ipAdress = "ec2-18-222-136-84.us-east-2.compute.amazonaws.com";
     public static final String serverPort = ":8080";
     public static final String urlBase = "/foodtruckgram";
 
